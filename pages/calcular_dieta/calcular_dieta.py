77382518d94c452d170682bdb59072337a3f62ed
Claudio Pickersgill <claudioppickersgill@gmail.com>
2026-03-11 20:03:54 -0300

    Teste pagina Calcular Dieta

---
+++ pages/calcular_dieta/calcular_dieta.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+from pages.conexao import cria_df
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+sns.set_style(rc={'axes.facecolor': '#0E1117',
+                  'axes.edgecolor': 'white',
+                  'axes.labelcolor': 'white',
+                  'text.color': 'white',
+                  'xtick.color': 'white',
+                  'ytick.color': 'white',
+                  'axes.grid': True,
+                  'grid.color': '.6',
+                  "grid.linestyle": ":"})
+fundo = '#0E1117'
+loc = 'upper right'
+
+def carregar_dieta():
+    df = cria_df.cria_df()
+
+    st.write(df.head())
